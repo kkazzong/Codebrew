ad19c204f1393d20bb00f9e98bb0fb3de87fec2a
GJ <GJ@DESKTOP-VJURLP7>
2017-10-16 09:05:31 +0900

    가정 : WebContent수정, Purchase 도메인, 매퍼, 서비스
    WebContent 폴더 추가(view, resources, common)
    Purchase.java
    QRCode.java
    PurchaseDAO.java
    PurchaseService.java

--- Codebrew/src/main/java/com/codebrew/moana/service/domain/QRCode.java
+++ Codebrew/src/main/java/com/codebrew/moana/service/domain/QRCode.java
@@ -2,8 +2,35 @@ package com.codebrew.moana.service.domain;
 
 public class QRCode {
 
+	// Field
+	private String qrCodeImage;
+	private String qrCodeResult;
+
+	// Constructor
 	public QRCode() {
-		// TODO Auto-generated constructor stub
+		System.out.println("QRCode default Constructor");
+	}
+
+	//getter, setter
+	public String getQrCodeImage() {
+		return qrCodeImage;
+	}
+
+	public void setQrCodeImage(String qrCodeImage) {
+		this.qrCodeImage = qrCodeImage;
+	}
+
+	public String getQrCodeResult() {
+		return qrCodeResult;
+	}
+
+	public void setQrCodeResult(String qrCodeResult) {
+		this.qrCodeResult = qrCodeResult;
+	}
+
+	@Override
+	public String toString() {
+		return "QRCode [qrCodeImage=" + qrCodeImage + ", qrCodeResult=" + qrCodeResult + "]";
 	}
 
 }
--- Codebrew/src/main/java/com/codebrew/moana/service/purchase/PurchaseService.java
+++ Codebrew/src/main/java/com/codebrew/moana/service/purchase/PurchaseService.java
@@ -1,5 +1,17 @@
 package com.codebrew.moana.service.purchase;
 
+import java.util.Map;
+
+import com.codebrew.moana.service.domain.Purchase;
+
 public interface PurchaseService {
 
+	public Purchase readyPayment(Purchase purchase);
+	public Purchase approvePayment(String pgToken);
+	public Purchase addPurchase(Purchase purchase);
+	public Map<String, Object> getPurchaseList(String userId, String purchaseFlag);
+	public Map<String, Object> getSaleList();
+	public Purchase getPurchase(int purchaseNo);
+	public void cancelPayment(int purchaseNo);
+	
 }
